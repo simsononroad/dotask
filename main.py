5ead10ba11d1c55b6a1a5e6beb0f772db69265be
simsononroad <d-github@kopaszhegy.hu>
2025-04-16 09:43:41 +0200

    ..

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from docx.shared import Mm
 from docxtpl import DocxTemplate, InlineImage
+import os
+import subprocess
 
 class Create_task():
     def __init__(self, file_name, file_desc, kerdes_valasz: list, feladatsor_neve):
@@ -12,22 +14,26 @@ class Create_task():
         a = input("(Ú)j kérdés, (G)enerálás, (K)ilépés")
         print()
         if a.upper() == 'Ú':
-            Create_task(self.file_name, self.file_desc, self.kerdes_valasz).new_task()
+            Create_task(self.file_name, self.file_desc, self.kerdes_valasz, self.feladatsor_neve).new_task()
         elif a.upper() == 'G':
-            Create_task(self.file_name, self.file_desc, self.kerdes_valasz).gen_file()
+            Create_task(self.file_name, self.file_desc, self.kerdes_valasz, self.feladatsor_neve).gen_file()
         else:exit()
     
     def gen_file(self):
         
         
         print(f"==================\n\tFájl neve: {self.file_name}\n\tFájl leírása: {self.file_desc}\n==================\n")
-        for feladatok in self.kerdes_valasz:
-            for key,value in dict(feladatok).items():
-                rossz_valaszok = value[0][0]
-                helyes_valaszok = value[1][0]
-                feladatsor_neve = self.feladatsor_neve
-                print(f"{key}:")
-                
+        
+        doc = DocxTemplate('base.docx')
+        context = {
+            'feladat_neve': self.feladatsor_neve,
+            'feladatsor_leirasa': self.file_desc,
+            'feladat_szam': 0,
+            'kerdes_valasz': self.kerdes_valasz
+        }
+        doc.render(context)
+        doc.save(self.file_name)
+        docx_2_pdf(self.file_name, "output.pdf")
                 
     def new_task(self):
         kerdes = input("Mi legyen a kérdés?\n>>>")
@@ -43,17 +49,29 @@ class Create_task():
         print(helyes_valasz_lista)
         self.kerdes_valasz.append({kerdes: [[valasz_lehetosegek_lista], [helyes_valasz_lista]]})
         print(self.kerdes_valasz)
-        Create_task(file_name=self.file_name, file_desc=self.file_desc, kerdes_valasz=self.kerdes_valasz).chose()
+        Create_task(file_name=self.file_name, file_desc=self.file_desc, kerdes_valasz=self.kerdes_valasz, feladatsor_neve=self.feladatsor_neve).chose()
         
         
             
 
 def main():
     a = input("Mi legyen a file neve?: ")
+    if '.docx' not in a:
+        a = f'{a}.docx'
     b = input("Mi legyen a feladatsor neve?: ")
     c = input("Mi legyen a feladatsor leírása?: ")
     Create_task(file_name=a, file_desc=c, kerdes_valasz=[], feladatsor_neve=b).chose()
    
+
+
+def docx_2_pdf(input_file, output_file):
+    subprocess.run(['libreoffice', '--headless', '--convert-to', 'pdf', input_file, '--outdir', 'tmp'], check=True)
+    print(f'tmp/{input_file[:-4]}pdf', {output_file})
+    os.rename(f'tmp/{input_file[:-4]}pdf', output_file)
+    os.remove(input_file)
+
+
+
 
 if __name__  == '__main__':
     main()
