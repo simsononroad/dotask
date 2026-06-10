571fcf9c3437a0a06b40ffab989c0e7c051815a8
simsononroad <d-github@kopaszhegy.hu>
2026-06-10 15:37:33 +0200

    add django

--- web/main.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template, redirect, url_for, flash
-
-app = Flask(__name__)
-app.route('/')
-def index():
-    return "szia"
-
-if __name__ == '__main__':
-    app.run(debug=True)
